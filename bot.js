54f4ec8435250afdc5bba74780fd370c777bb388
alirhaby2018 <46156691+alirhaby2018@users.noreply.github.com>
2019-03-25 21:22:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`ãíÊ ÏÇÎáíÇ`,'https://www.twitch.tv/By:A7MD');
+  client.user.setGame(`Ù…ÙŠØª Ø¯Ø§Ø®Ù„ÙŠØ§`,'https://www.twitch.tv/By:A7MD');
   console.log('BOT ONLINE');
 });
-client.login('NDU1Mjg4Mzk4MjQ5MzI4NjUw.D3qryw.hcv1H3wwx75-r5W80b4rafqK-Dc');
+client.login(process.env.BOT_TOKEN);
