57c78fce8623ddf64fe5fe38b351318dd021e080
alirhaby2018 <46156691+alirhaby2018@users.noreply.github.com>
2019-03-25 21:20:10 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`„Ì  œ«Œ·Ì«`,'https://www.twitch.tv/By:A7MD');
+  console.log('BOT ONLINE');
+});
+client.login('NDU1Mjg4Mzk4MjQ5MzI4NjUw.D3qryw.hcv1H3wwx75-r5W80b4rafqK-Dc');
