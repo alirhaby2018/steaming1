569c21234bd77e21a4cfa0fe3306abc27f3d8591
alirhaby2018 <46156691+alirhaby2018@users.noreply.github.com>
2019-03-25 21:34:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`ميت داخليا`,'https://www.twitch.tv/By:A7MD');
+  client.user.setGame(`♔الملك♔`,'https://www.twitch.tv/By:A7MD');
   console.log('BOT ONLINE');
 });
 client.login(process.env.BOT_TOKEN);
